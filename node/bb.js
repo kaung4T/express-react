eea7c835dd536fc2069fda2ff296f6f6c9602bba
kaungkaung4T <mrkaungminnkhant@gmail.com>
2023-07-08 04:51:54 +0630

    update

--- node/bb.js
+++ node/bb.js
@@ -4,7 +4,6 @@
 
 let express = require("express")
 let mysql = require("mysql")
-
 let app = express()
 
 app.use(express.json())
@@ -16,60 +15,56 @@ let db = mysql.createConnection({
     database: "backend_item"
 })
 
-
-app.get("/item", item)
-function item (request, response) {
-    let item = "SELECT * FROM item WHERE name=?"
-    db.query(item, ["eeee"], (error, data) => {
+app.get("/item", (request, response) => {
+    let item_query = "SELECT * FROM item"
+    db.query(item_query, (error, data) => {
         if (error) {response.json(error)}
         response.json(data)
     })
-}
+})
 
 
-app.post("/create_item", create_item)
-function create_item (request, response) {
+app.post("/create_item", (request, response) => {
     let name = request.body.name
     let price = request.body.price
-    let create = "INSERT INTO item (name, price) VALUES (?)"
-    values = [
+    let create_query = "INSERT INTO item (name, price) VALUES (?)"
+    let values = [
         name,
         price
     ]
-    db.query(create, [values], (error, data) => {
+    db.query(create_query, [values], (error, data) => {
         if (error) {response.json(error)}
         response.json(data)
     })
-}
+})
 
 
-app.put("/update/:id", update_item)
-function update_item (request, response) {
+app.put("/update/:id", (request, response) => {
     let id = request.params.id
     let name = request.body.name
     let price = request.body.price
-    let update = "UPDATE item SET name=?, price=? WHERE id=?"
-    values = [
+    let update_query = "UPDATE item SET name=?, price=? WHERE id=?"
+    let values = [
         name,
         price
     ]
-    db.query(update, [...values, id], (error, data) => {
+    db.query(update_query, [...values, id], (error, data) => {
         if (error) {response.json(error)}
         response.json(data)
     })
-}
+})
 
 
-app.post("/delete/:id", delete_item)
-function delete_item (request, response) {
+app.post("/delete/:id", (request, response) => {
     let id = request.params.id
 
-    let delete_query = "DELETE FROM item WHERE id=?"
+    let create_query = "DELETE FROM item WHERE id=?"
 
-    db.query(delete_query, [id], (error, data) => {
+    db.query(create_query, [id], (error, data) => {
         if (error) {response.json(error)}
         response.json(data)
     })
-}
+})
+
 
 app.listen(5000)
